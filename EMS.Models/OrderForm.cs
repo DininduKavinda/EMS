48ebb29d86605804cc384303830ea962a4bb03bb
DininduKavinda <kavinda373@gmail.com>
2024-01-20 16:53:07 +0530

    main

---
+++ EMS.Models/OrderForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Models
+{
+    public class OrderForm
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Order Form No")]
+        public int OrderForm_No { get; set; }
+        [Required]
+        [DisplayName("Customer Name")]
+        public int CustomerId { get; set; }
+        [Required]
+        [DisplayName("Sales Executive")]
+        public int SalesExecutiveId { get; set; }
+        [Required]
+        [DisplayName("Sub Route")]
+        public string? SubRoute { get; set; }
+        [Required]
+        [DisplayName("Road")]
+        public string? Road { get; set; }
+        [Required]
+        [DisplayName("Order Form Date")]
+        public DateTime OrderForm_Date { get; set; }
+        [Required]
+        [DisplayName("Required Date")]
+        public DateTime OrderRequired_Date { get; set; }
+        [ForeignKey("CustomerId")]
+        public Customer? Customer { get; set; }
+        [ForeignKey("SalesExecutiveId")]
+        public SalesExecutive? SalesExecutive { get; set; }
+    }
+}
---
+++ EMS.Models/OrderFormProducts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Models
+{
+    public class OrderFormProducts
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Order Form No")]
+        public int OrderForm_Id { get; set; }
+        [Required]
+        [DisplayName("Product")]
+        public int ProductId { get; set; }
+        [Required]
+        [DisplayName("Required Quantity")]
+        public int Required_Quantity { get; set; }
+    }
+}
