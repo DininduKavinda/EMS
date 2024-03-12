96f7310a847efeb0648444d0b030b9ca7a71ac2f
DininduKavinda <kavinda373@gmail.com>
2024-03-12 15:27:26 +0530

    main

--- EMS/Areas/Distribution/Controllers/DistributionPointController.cs
+++ EMS/Areas/Distribution/Controllers/DistributionPointController.cs
@@ -1,5 +1,6 @@
 ﻿using EMS.DataAccess.Repository.IRepository;
 using EMS.Models;
+using EMS.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,12 +21,54 @@ namespace EMS.Web.Areas.Distribution.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Upsert(DistributionPoint distributionPoint)
+        {
+            if (ModelState.IsValid)
+            {
+                if (distributionPoint.Id == 0)
+                {
+                    _unitOfWorks.DistributionPoint.Add(distributionPoint);
+                }
+                else
+                {
+                    _unitOfWorks.DistributionPoint.Update(distributionPoint);
+                }
+                _unitOfWorks.Save();
+                TempData["success"] = "Update Successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             List<DistributionPoint> distributionPointsList = _unitOfWorks.DistributionPoint.GetAll().ToList();
             return Json(new { data = distributionPointsList });
+        }
+        [HttpGet]
+        public IActionResult getByID(int id)
+        {
+            DistributionPoint dbpoint = new DistributionPoint();
+            dbpoint = _unitOfWorks.DistributionPoint.Get(u => u.Id == id);
+            return Json(new { data = dbpoint });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var DistributionPointToBeDeleted = _unitOfWorks.DistributionPoint.Get(u => u.Id == id);
+            if (DistributionPointToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error While Deleting" });
+            }
+            _unitOfWorks.DistributionPoint.Remove(DistributionPointToBeDeleted);
+            _unitOfWorks.Save();
+            List<DistributionPoint> objDBPList = _unitOfWorks.DistributionPoint.GetAll().ToList();
+            return Json(new { data = objDBPList, success = true, message = "Deleted Succesfully" });
         }
         #endregion
     }
---
+++ EMS/wwwroot/js/DistributionPoint.js
@@ -0,0 +1,69 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/distribution/distributionPoint/getAll'
+        },
+        columns: [
+            { data: 'name' },
+            { data: 'address' },
+            { data: 'description' },   
+            {
+                data: 'id',
+                render: function (data) {
+                    return `
+                    <div class="w-75 btn-group" role="group">
+                    <a href="/distribution/distributionPoint/upsert?id=${data}" data-id="${data}" data-toggle="modal" data-target="#exampleModal" class="btn btn-warning mx-2  edit-button">Edit</a>
+                    <a onClick=Delete('/distribution/distributionPoint/delete?id=${data}') class="btn btn-danger mx-2">Delete</a>
+                    </div>
+                    `;
+                }
+            }
+        ]
+    });
+}
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
+}
+$(document).on('click', '.edit-button', function () {
+    var id = $(this).data('id');
+    $.ajax({
+        url: '/distribution/distributionPoint/getById?id=' + id,
+        type: 'GET',
+        success: function (result) {
+            $("#exampleModal input[name='Id']").val(result.data.id);
+            $("#exampleModal input[name='Name']").val(result.data.name);
+            $("#exampleModal input[name='Address']").val(result.data.address);
+            $("#exampleModal input[name='Description']").val(result.data.description);
+            $("#exampleModal").modal({ backdrop: 'static' });
+        },
+        error: function (error) {
+            console.log(error);
+            $("#exampleModal").modal('close');
+        }
+    });
+});
+
